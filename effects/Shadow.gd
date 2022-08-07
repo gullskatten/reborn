9125c7d3ff0b33bfb5bdf38bd626d61e0daad1b6
Espen G <espen_gud@msn.com>
2022-08-07 12:13:55 +0200

    added shadows to most objects, started rewriting for 3.5

---
+++ effects/Shadow.gd
@@ -0,0 +1,8 @@
+extends Sprite
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	pass
--- structures/props/vegetation/SingleStraw.gd
+++ structures/props/vegetation/SingleStraw.gd
@@ -4,7 +4,9 @@ onready var sprite = $Straw
 onready var wildSwayShader = preload("res://assets/shaders/SwayWild.shader")
 
 func _ready():
-	sprite.frame = rand_range(0, sprite.vframes * sprite.hframes-1)
+	var rand_sprite_frame = rand_range(0, sprite.vframes * sprite.hframes-1)
+	sprite.frame = rand_sprite_frame
+	$Shadow.frame = rand_sprite_frame
 	
 func reset_shader():
 	sprite.material.set_shader_param("speed", 1)
@@ -17,7 +19,6 @@ func _on_Bush2_finished():
 	reset_shader()
 
 func _on_SoftCollisions_body_entered(_body):
-	print("something hit!")
 	print(_body)
 	sprite.material.set_shader_param("speed", 20)
 	sprite.material.set_shader_param("max_strength", 0.572)
