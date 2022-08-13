1c0c597bcd7d8ecd0adb693f5385aea96628360a
Espen G <espen_gud@msn.com>
2022-08-13 23:56:11 +0200

    added a bush vegetation and tween for sway, skull pole scare crow

--- structures/props/vegetation/SingleStraw.gd
+++ structures/props/vegetation/SingleStraw.gd
@@ -4,13 +4,13 @@ onready var sprite = $Straw
 onready var wildSwayShader = preload("res://assets/shaders/SwayWild.shader")
 
 func _ready():
-	var rand_sprite_frame = rand_range(0, sprite.vframes * sprite.hframes-1)
+	var rand_sprite_frame = rand_range(0, sprite.vframes * sprite.hframes)
 	sprite.frame = rand_sprite_frame
 	$Shadow.frame = rand_sprite_frame
 	
 func reset_shader():
-	sprite.material.set_shader_param("speed", 1)
-	sprite.material.set_shader_param("max_strength", 0.100)
+	sprite.material.set_shader_param("speed", 1.0)
+	sprite.material.set_shader_param("maxStrength", 0.100)
 
 func _on_Bush1_finished():
 	reset_shader()
@@ -19,9 +19,8 @@ func _on_Bush2_finished():
 	reset_shader()
 
 func _on_SoftCollisions_body_entered(_body):
-	print(_body)
-	sprite.material.set_shader_param("speed", 20)
-	sprite.material.set_shader_param("max_strength", 0.572)
+	sprite.material.set_shader_param("speed", 12.5)
+	sprite.material.set_shader_param("maxStrength", 0.172)
 	
 	if randi() % 2 == 0:
 		$Bush1.play()
